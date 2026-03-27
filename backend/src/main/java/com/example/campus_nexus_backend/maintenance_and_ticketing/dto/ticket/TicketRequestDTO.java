812389624f473a7d2dd6dcf8a9e25240447007d3
Sahiru <mwsahirubro75@gmail.com>
2026-03-27 16:17:35 +0530

    created ticket DTOs

---
+++ backend/src/main/java/com/example/campus_nexus_backend/maintenance_and_ticketing/dto/ticket/TicketRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.campus_nexus_backend.maintenance_and_ticketing.dto.ticket;
+
+import lombok.Data;
+
+@Data
+public class TicketRequestDTO {
+    private Long resourceId;
+    private String locationText;
+    private String category;
+    private String description;
+    private String priority;
+    private String preferredContact;
+}
---
+++ backend/src/main/java/com/example/campus_nexus_backend/maintenance_and_ticketing/dto/ticket/TicketResponseDTO.java
@@ -0,0 +1,22 @@
+package com.example.campus_nexus_backend.maintenance_and_ticketing.dto.ticket;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class TicketResponseDTO {
+    private Long ticketId;
+    private Long resourceId;
+    private String locationText;
+    private String category;
+    private String description;
+    private String priority;
+    private String preferredContact;
+    private String status;
+    private String resolutionNotes;
+    private LocalDateTime createdAt;
+    
+    // To send attachment details back to the frontend
+    private List<AttachmentDTO> attachments;
+}
