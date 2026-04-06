fc2dda8a3227bf78e2bdbbec7aa16866c6b38419
Sahiru <mwsahirubro75@gmail.com>
2026-04-06 11:54:47 +0530

    refactor ResourceRepository to remove unused import and update application.properties for correct .env file path

--- backend/src/main/java/com/example/campus_nexus_backend/facilities/repository/ResourceRepository.java
+++ backend/src/main/java/com/example/campus_nexus_backend/facilities/repository/ResourceRepository.java
@@ -1,7 +1,6 @@
 package com.example.campus_nexus_backend.facilities.repository;
 
 import com.example.campus_nexus_backend.facilities.model.ResourcesModel;
-import com.nimbusds.jose.util.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResourceRepository extends JpaRepository<ResourcesModel, Long> {
